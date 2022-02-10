586472882a8b530c26c19a3ec3bba99b04a71010
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2022-02-11 02:55:46 +0700

    add: about activity & update: data static planet

--- app/src/main/java/com/rendiputra/pengenalanbintangapp/MainActivity.kt
+++ app/src/main/java/com/rendiputra/pengenalanbintangapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.rendiputra.pengenalanbintangapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -24,5 +27,23 @@ class MainActivity : AppCompatActivity() {
         rvPlanets.layoutManager = LinearLayoutManager(this)
         val homeCardView = HomeCardView(list)
         rvPlanets.adapter = homeCardView
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.miCompose -> {
+                val about = Intent(this@MainActivity, About::class.java)
+                startActivity(about)
+            }
+        }
     }
 }
